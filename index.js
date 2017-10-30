a15b48dabc8c7971b59ff7b1eeb692d5a38dc5a9
Chris <iamchris88@live.co.uk>
2017-10-30 07:57:42 +0000

    added basic route set-ups

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ var express = require('express');
 var app = express();
 var bodyparse = require('body-parser');
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service');
-//mongoose.connect('mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-servce');
 
-app.get('/', function(req,res){
-    res.send('hello')
-});
-
-
-
+app.use('/purchasing', require('./routes/purchase'));
 app.listen(3000);
