6d0e8155827cb53082613fe5cba6fc03219dee69
Chris <iamchris88@live.co.uk>
2017-10-30 09:16:50 +0000

    pulling suppliers and allowing them to be clicked

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ var bodyparse = require('body-parser');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service');
+mongoose.Promise = global.Promise;
+
+app.use(express.static('public'));
+app.use(bodyparse.json());
+app.use(bodyparse.urlencoded({extended: true}));
+
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 
 app.use('/purchasing', require('./routes/purchase'));
-app.listen(8000);
+
+app.use(function(err, req, res, next){
+    res.status(422).send({err: err.message});
+})
+app.listen(process.env.port || 5000);
--- routes/purchase.js
+++ routes/purchase.js
@@ -2,16 +2,23 @@ var express = require('express');
 var router = express.Router();
 const Supplier = require('../models/suppliers.js');
 
-//I should list all the suppliers
-router.get('/', function(req, res, next){
 
-  Supplier.find({}).then(function(doc){
-      res.send(doc);
-  })
+//gets a list of customers by the custid passed in (or id if they themselves are customers)
+router.get('/',function(req, res,next){
+    Supplier.find({
+    }).then(function(suppliers){
+        res.render('viewSuppliers', {supplierlist : suppliers});
+    });
 });
+
+
 
 //I should list all the products from a supplier
 router.get('/:id', function(req,res,next){
+    Supplier.find({ name : req.params.id
+    }).then(function(supplier){
+        res.send(supplier);
+    });
 })
 
 router.post('/autopurchase', function(req,res,next){
