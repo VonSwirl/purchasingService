3be4be90a92f3f112b72639953f6eccd478fb02f
Chris <iamchris88@live.co.uk>
2017-11-02 17:04:51 +0000

    sending order request to the purchasing dept

--- routes/purchase.js
+++ routes/purchase.js
@@ -6,41 +6,50 @@ const Supplier = require('../models/suppliers.js');
 const Product = require('../models/product.js');
 const ProductSupply = require('../models/productsupply.js');
 const StockUpdater = require('../services/supplierStockUpdater.js');
-const StockDetailGrabber = require('../services/supplierStockDetailGrabber.js');
-
+const request = require('request');
 
 
 //gets a list of customers by the custid passed in (or id if they themselves are customers)
 router.get('/',function(req, res,next){
-    Supplier.find({
-    }).then(function(suppliers){
-        res.render('viewSuppliers', {supplierlist : suppliers});
-    });
-});
-
-router.get('/chooseSupplier', function(req, res,next){
-    for(var propName in req.query){
-        if(req.query.hasOwnProperty(propName)){
-            console.log(propName);
-            if(req.query[propName]){
-                var productSupplies = [];
-                console.log(propName);
-                ProductSupply.find({Ean : propName}).then(function(supplier){
-                    console.log(supplier);
-                });
-                }
-            }
-        }
-        res.send('finished');
-})
-
-
-router.get('/updateSupplierStockDetails', function(req,res,next){
-    StockUpdater.updateDbWithAllSupplierStockDetails();
-
     Product.find({}).populate({path: 'suppliersThatStock', match: {inStock : true}}).lean().exec(function (err, product) {
         res.render('viewProducts.pug', {productList : product});
     });
+});
+
+router.post('/test', function(req,res,next){
+
+    console.log(req.body);
+});
+
+router.get('/submitOrder', function(req,res,next){
+    for (var propName in req.query) {
+        if (req.query.hasOwnProperty(propName)) {
+
+           
+            if (req.query[propName][1] != '' && parseInt(req.query[propName][1]) > 0 && req.query[propName][2] != 'Select Supplier' ) {
+                var item = {'name' : req.query[propName][0],
+                            'numberRequired' : req.query[propName][1],
+                            'ean' : propName,
+                            'suppliername' : req.query[propName][2]
+                }
+                request.post({
+                    url : "http://localhost:4000/purchasing/test",
+                    body: item,
+                    json: true
+                });
+            }
+        }
+    }
+
+    res.send('order sucessful page .... I need to be done, in good news, the post was successful');
+
+
+});
+
+
+router.put('/updateSupplierStockDetails', function(req,res,next){
+    StockUpdater.updateDbWithAllSupplierStockDetails();
+
 });
 
 router.post('/autopurchase', function(req,res,next){
