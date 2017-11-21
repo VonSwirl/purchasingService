a27a46a1d7ab4936b08414eba7f22525f4d66cab
Chris <iamchris88@live.co.uk>
2017-11-21 11:36:35 +0000

    update  config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = {}
 
 config.databaseURL = 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service';
-config.orderServiceURLtoUpdateWithPurchase = "http://localhost:3001/purchasing/test"; //"http://localhost:3004/PurchasingUpdate";
+config.orderServiceURLtoUpdateWithPurchase = "https://peaceful-caverns-91545.herokuapp.com/purchasing/test"; //"http://localhost:3004/PurchasingUpdate";
 config.testDatabaseURL = "mongodb://localhost:27017/MyDb";
-config.AdminServicePurchaseURL = "http://localhost:3001/purchasing/test";
-config.stockServiceUpdaterURL= "http://localhost:3001/purchasing/test";
+config.AdminServicePurchaseURL = "https://peaceful-caverns-91545.herokuapp.com/purchasing/test";
+config.stockServiceUpdaterURL= "https://peaceful-caverns-91545.herokuapp.com/purchasing/test";
 module.exports = config;
