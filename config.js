24a94d6ed3ed07ccdca37c1b4df03bdeca423127
Chris <iamchris88@live.co.uk>
2017-11-27 17:04:39 +0000

    Added search and filter in the purchasing front end

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ config.orderServiceURLtoUpdateWithPurchase = "https://localhost:3004/order/Purch
 config.testDatabaseURL = "mongodb://pserv:pserv1@ds121726.mlab.com:21726/test-purchase-service";
 config.AdminServicePurchaseURL = "https://peaceful-caverns-91545.herokuapp.com/purchasing/test";
 config.stockServiceUpdaterURL= "https://localhost:3003/api/newproduct";
-congig.secret = 'jwtsecret'
+config.secret = 'jwtsecret'
 module.exports = config;
