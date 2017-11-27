31a1f9c9d83e8fd0da2e08dadda07494ac53c2b3
Jerome Hurley <vonswirl@googlemail.com>
2017-11-27 12:58:03 +0000

    remove all

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.databaseURL = 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service';
-config.orderServiceURLtoUpdateWithPurchase = "https://peaceful-caverns-91545.herokuapp.com/purchasing/test"; //"http://localhost:3004/PurchasingUpdate";
-config.testDatabaseURL = "mongodb://localhost:27017/MyDb";
-config.AdminServicePurchaseURL = "https://peaceful-caverns-91545.herokuapp.com/purchasing/test";
-config.stockServiceUpdaterURL= "https://peaceful-caverns-91545.herokuapp.com/purchasing/test";
-module.exports = config;
