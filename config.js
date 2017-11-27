7559a803937478b66d266bf166ab5ad2ce7a5f65
Chris <iamchris88@live.co.uk>
2017-11-27 17:49:53 +0000

    updated filtering for the purchasing page

--- config.js
+++ config.js
@@ -4,10 +4,6 @@ config.databaseURL = 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-se
 config.orderServiceURLtoUpdateWithPurchase = "https://localhost:3004/order/PurchasingUpdate"; //"http://localhost:3004/PurchasingUpdate";
 config.testDatabaseURL = "mongodb://pserv:pserv1@ds121726.mlab.com:21726/test-purchase-service";
 config.AdminServicePurchaseURL = "https://peaceful-caverns-91545.herokuapp.com/purchasing/test";
-config.stockServiceUpdaterURL= "https://localhost:3003/api/newproduct";
-<<<<<<< HEAD
+config.stockServiceUpdaterURL= "https://peaceful-caverns-91545.herokuapp.com/purchasing/test";
 config.secret = 'jwtsecret'
-=======
-congig.secret = 'jwtsecret'
->>>>>>> a42a3551371004f902e01cf2971c9bffbdb0bba8
 module.exports = config;
