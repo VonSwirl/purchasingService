b18324475c678b0c1d8ac13c7c131707209cec93
Chris <p4183397@live.tees.ac.uk>
2017-12-04 18:28:33 +0000

    Added to the config file

--- config.js
+++ config.js
@@ -1,36 +1,42 @@
 
 
-var development = {'databaseURL' : 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service',
-'orderServiceURLtoUpdateWithPurchase' : "http://3amigoso.azurewebsites.net/order/PurchasingUpdate/", //"http://localhost:3004/PurchasingUpdate";
-'testDatabaseURL' : "mongodb://pserv:pserv1@ds121726.mlab.com:21726/test-purchase-service",
-'AdminServicePurchaseURL' : "https://peaceful-caverns-91545.herokuapp.com/purchasing/test",
-'stockServiceUpdaterURL' : "http://3amigoss.azurewebsites.net/api/newproducts",
-'secret' : 'jwtsecret'}
+var development = {
+    'databaseURL': 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service',
+    'orderServiceURLtoUpdateWithPurchase': "http://3amigoso.azurewebsites.net/order/PurchasingUpdate/", //"http://localhost:3004/PurchasingUpdate";
+    'testDatabaseURL': "mongodb://pserv:pserv1@ds121726.mlab.com:21726/test-purchase-service",
+    'AdminServicePurchaseURL': "https://peaceful-caverns-91545.herokuapp.com/purchasing/test",
+    'stockServiceUpdaterURL': "http://3amigoss.azurewebsites.net/api/newproducts",
+    'secret': 'jwtsecret'
+}
 
-var test = {'databaseURL' : 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service',
-'orderServiceURLtoUpdateWithPurchase' : "http://3amigoso.azurewebsites.net/order/PurchasingUpdate/", //"http://localhost:3004/PurchasingUpdate";
-'testDatabaseURL' : "mongodb://pserv:pserv1@ds121726.mlab.com:21726/test-purchase-service",
-'AdminServicePurchaseURL' : "https://peaceful-caverns-91545.herokuapp.com/purchasing/test",
-'stockServiceUpdaterURL' : "http://3amigoss.azurewebsites.net/api/newproducts",
-'secret' : 'jwtsecret'}
+var test = {
+    'databaseURL': 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service',
+    'orderServiceURLtoUpdateWithPurchase': "http://3amigoso.azurewebsites.net/order/PurchasingUpdate/", //"http://localhost:3004/PurchasingUpdate";
+    'testDatabaseURL': "mongodb://pserv:pserv1@ds121726.mlab.com:21726/test-purchase-service",
+    'AdminServicePurchaseURL': "https://peaceful-caverns-91545.herokuapp.com/purchasing/test",
+    'stockServiceUpdaterURL': "http://3amigoss.azurewebsites.net/api/newproducts",
+    'secret': 'jwtsecret'
+}
 
-var standard = {'databaseURL' : 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service',
-'orderServiceURLtoUpdateWithPurchase' : "http://3amigoso.azurewebsites.net/order/PurchasingUpdate/", //"http://localhost:3004/PurchasingUpdate";
-'testDatabaseURL' : "mongodb://pserv:pserv1@ds121726.mlab.com:21726/test-purchase-service",
-'AdminServicePurchaseURL' : "https://peaceful-caverns-91545.herokuapp.com/purchasing/test",
-'stockServiceUpdaterURL' : "http://3amigoss.azurewebsites.net/api/newproducts",
-'secret' : 'jwtsecret'}
+var standard = {
+    'databaseURL': 'mongodb://pserv:pserv1@ds241055.mlab.com:41055/purchase-service',
+    'orderServiceURLtoUpdateWithPurchase': "http://3amigoso.azurewebsites.net/order/PurchasingUpdate/", //"http://localhost:3004/PurchasingUpdate";
+    'testDatabaseURL': "mongodb://pserv:pserv1@ds121726.mlab.com:21726/test-purchase-service",
+    'AdminServicePurchaseURL': "https://peaceful-caverns-91545.herokuapp.com/purchasing/test",
+    'stockServiceUpdaterURL': "http://3amigoss.azurewebsites.net/api/newproducts",
+    'secret': 'jwtsecret'
+}
 
-var config =function (){
-    switch(process.env.NODE_ENV){
-    case 'development':
-    return development;
-    case 'standard':
-    return standard;
-
-    default:
-        return development;
-}}
+var config = function () {
+    switch (process.env.NODE_ENV) {
+        case 'development':
+            return development;
+        case 'standard':
+            return standard;
+        default:
+            return development;
+    }
+}
 
 
 module.exports = config();
